6dd0e8b1329704d8c66a6839a5de253961fea845
DustinG98 <thedustingraham@gmail.com>
2020-02-21 19:36:23 -0700

    login, axioswithauth, private route, display colors, finish edit and delete colors

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from 'axios'
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChanges = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/login', credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            // history.push('/friends');
+        })
+        .catch(err => console.log(err))
+}
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={e => handleSubmit(e)}>
+          <input name="username" type="text" value={credentials.username} onChange={e => handleChanges(e)}/>
+          <input name="password" type="password" value={credentials.password} onChange={e => handleChanges(e)}/>
+          <button type="submit">Login</button>
+      </form>
     </>
   );
 };
